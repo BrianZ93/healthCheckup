642d264815c1e0251085ac9ba2fe540e5e6d01e7
Brian Zanayed <BrianZanayed@gmail.com>
2022-11-26 22:08:16 -0600

    java setup

--- backend-java-api/main.java
+++ backend-java-api/main.java
@@ -1,3 +1,22 @@
-package main
+import java.io.File;
 
-public public static void main(String[]args){System.out.Println("hi")}
+class Main {
+    public static void main(String[] args) {
+
+        // Create a file object fort he current location
+        File file = new File("recipes.txt");
+
+        try {
+
+            // trying to create a file based on the object
+            boolean value = file.createNewFile();
+            if (value) {
+                System.out.println("The new file is created.");
+            } else {
+                System.out.println("The file already exists.");
+            }
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+    }
+}
